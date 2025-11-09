1014bccfa80b0df49df17415325695550fb45ce3
phuclee <phucle682004@gmail.com>
2025-11-10 00:59:35 +0700

    fix metu

--- src/components/layout/Metu.jsx
+++ src/components/layout/Metu.jsx
@@ -1,28 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Metu.css";
-import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneVolume, faHome } from "@fortawesome/free-solid-svg-icons";
 import { Zalo } from "../../assets/icons/icons";
+import { Link } from "react-router-dom";
 
 const Metu = () => {
   return (
     <div id="metu">
-      <div>
-        <a href="">
-          <FontAwesomeIcon icon={faPhoneVolume} />
-          0788580223
-        </a>
-      </div>
-      <div className="metu-mid">
-        <a href="">
-          <Zalo className="icon-zalo" />
-          Nhắn tin Zalo
-        </a>
-      </div>
-      <div>
-        <a href="">
-          <FontAwesomeIcon icon={faPhoneVolume} />
-          0788580223
-        </a>
+      <div className="container">
+        <div>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome} />
+            <span>Trang chủ</span>
+          </Link>
+        </div>
+        <div className="metu-mid">
+          <a
+            href="https://zalo.me/0788580223"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Zalo className="icon-zalo" />
+            <span>Nhắn tin Zalo</span>
+          </a>
+        </div>
+        <div>
+          <a href="tel:0788580223">
+            <FontAwesomeIcon icon={faPhoneVolume} />
+            <span>0788580223</span>
+          </a>
+        </div>
       </div>
     </div>
   );
