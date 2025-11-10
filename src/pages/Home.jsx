ce5d20b034f5e9027ef9c17a8d7b1dbdeef22018
phuclee <phucle682004@gmail.com>
2025-11-10 15:59:10 +0700

    HomePage basic và slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import Slider from "../components/layout/Slider.jsx";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="main">
+      <div className="container">
+        <Slider />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
